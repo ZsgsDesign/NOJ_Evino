be423802e49d8b18f016f29f02db7a241f728fb9
ZsgsDesign <zsgsdesign@gmail.com>
2021-11-10 14:48:11 +0800

    add version component

---
+++ src/components/version/VersionComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <p class="text-grey font-poppins evino__version_badge">
+        <q-icon name="mdi-google-downasaur" /> <span class="text-grey-5">v1.0.0.19</span>-<span class="text-grey-5">dev</span> <span class="text-grey-6 text-italic text-bold text-underline">Alwalkeria</span> <span class="text-grey-7 text-italic">Three Year Anniversary Build Pack</span>
+    </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'VersionComponent',
+});
+</script>
+
+<style lang="scss" scoped>
+.evino {
+    &__version_badge {
+        span {
+            word-break: keep-all;
+            white-space: nowrap;
+        }
+    }
+}
+</style>
