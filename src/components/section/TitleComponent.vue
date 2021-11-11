11d5675b074e147b9e8cb075cae79cd8ae58e07c
ZsgsDesign <zsgsdesign@gmail.com>
2021-11-11 21:23:45 +0800

    add section title component

---
+++ src/components/section/TitleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <p class="text-bold text-grey-7"><q-icon :name="icon"></q-icon> {{title}}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name: 'SectionTitle',
+    props: {
+        icon: {
+            default: () => 'format-section',
+        },
+        title: {
+            default: () => '',
+        },
+    },
+    setup() {
+        return {
+            slide: ref(1),
+        }
+    },
+});
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,16 @@
 <template>
-    <q-page class="container row">
-        <div class="col-12 col-md-8 q-pa-md">
-            <h4 class="q-mt-md q-mb-xs">Welcome to {{defaultNOJConfig.app.name}}!</h4>
-            <version-component></version-component>
-            <carousel-component title="Home" active :carousels="carousels"></carousel-component>
+    <q-page class="container q-pt-md fit">
+        <div class="row">
+            <div class="col-12 col-md-8 q-pa-md">
+                <div class="q-mb-lg">
+                    <h4 class="q-mt-none q-mb-xs">Welcome to {{ defaultNOJConfig.app.name }}!</h4>
+                    <version-component />
+                </div>
+                <carousel-component title="Home" active :carousels="carousels" />
+            </div>
+            <div class="col-12 col-md-4 q-pa-md">
+                <section-title icon="mdi-puzzle" title="Installed Extensions" />
+            </div>
         </div>
     </q-page>
 </template>
@@ -12,16 +19,17 @@
 import { Carousel } from 'components/models';
 import CarouselComponent from 'components/home/CarouselComponent.vue';
 import VersionComponent from 'components/version/VersionComponent.vue';
+import SectionTitle from 'components/section/TitleComponent.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
     name: 'HomePage',
-    components: { CarouselComponent, VersionComponent },
+    components: { CarouselComponent, VersionComponent, SectionTitle },
     setup() {
         const carousels = ref<Carousel[]>([
             {
                 order: 1,
-                url: 'https://cdn.quasar.dev/img/mountains.jpg',
+                url: 'https://images.unsplash.com/photo-1554034483-04fda0d3507b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80',
             },
             {
                 order: 2,
